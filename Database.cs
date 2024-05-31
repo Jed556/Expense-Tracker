2434e329a6d14c5adcfde508a84c4177a25bca29
Jed556 <jguiriba11@gmail.com>
2024-05-31 16:28:44 +0800

    feat: Added database handling

--- Database.cs
+++ Database.cs
@@ -1,19 +1,130 @@
 ﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace MyEnterpriseSystem
+namespace ExpenseTracker
 {
-    class Database
+     class Database
     {
-        public String connectionString;
-        public Database()
+
+        private String connectionString = "";
+        public MySqlConnection connection;
+
+        public Database(String connectionString)
         {
-            connectionString = "datasource=localhost;port=3306;Initial Catalog='expensetracker';username=root;password=";
+            this.connectionString = connectionString;
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                if (this.connection.State == ConnectionState.Closed)
+                {
+                    this.connection.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Connnecting to Database: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                FrmLogin FrmLogin = new FrmLogin();
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (this.connection.State == ConnectionState.Open)
+            {
+                this.connection.Close();
+            }
+        }
+
+        public DataTable ExecuteAdapter(String Query)
+        {
+            Connect();
+            MySqlDataAdapter mda = new MySqlDataAdapter();
+            DataTable dt = new DataTable();
+            BindingSource bs = new BindingSource();
+
+            MySqlCommand command = new MySqlCommand(Query, this.connection);
+            mda.SelectCommand = command;
+            mda.Fill(dt);
+            bs.DataSource = dt;
+
+            return dt;
+        }
+
+        public int ExecuteQuery(String Query, Expense Schema)
+        {
+            Connect();
+
+            MySqlCommand command = new MySqlCommand(Query, this.connection);
+            command.Parameters.AddWithValue("@ID", Schema.id);
+            command.Parameters.AddWithValue("@userID", Schema.userId);
+            command.Parameters.AddWithValue("@Name", Schema.name);
+            command.Parameters.AddWithValue("@Amount", Schema.amount);
+            command.Parameters.AddWithValue("@Date", Schema.date);
+
+            int success = command.ExecuteNonQuery();
+
+            Disconnect();
+
+            return success;
+        }
+
+        public int ExecuteQuery(String Query, User Schema)
+        {
+            Connect();
+
+            MySqlCommand command = new MySqlCommand(Query, this.connection);
+            command.Parameters.AddWithValue("@ID", Schema.id);
+            command.Parameters.AddWithValue("@Username", Schema.username);
+            command.Parameters.AddWithValue("@Password", Schema.password);
+
+            int success = command.ExecuteNonQuery();
+
+            Disconnect();
+
+            return success;
+        }
+
+        public MySqlDataReader SearchQuery(String Query, Expense Schema)
+        {
+            Connect();
+
+            MySqlCommand command = new MySqlCommand(Query, this.connection);
+            command.Parameters.AddWithValue("@ID", Schema.id);
+            command.Parameters.AddWithValue("@userID", Schema.id);
+            command.Parameters.AddWithValue("@Name", Schema.name);
+            command.Parameters.AddWithValue("@Amount", Schema.amount);
+            command.Parameters.AddWithValue("@Date", Schema.date);
+
+            MySqlDataReader mdr = command.ExecuteReader();
+
+            return mdr;
+        }
+
+        public MySqlDataReader SearchQuery(String Query, User Schema)
+        {
+            Connect();
+
+            MySqlCommand command = new MySqlCommand(Query, this.connection);
+            command.Parameters.AddWithValue("@ID", Schema.id);
+            command.Parameters.AddWithValue("@Username", Schema.username);
+            command.Parameters.AddWithValue("@Password", Schema.password);
+
+            MySqlDataReader mdr = command.ExecuteReader();
+
+            return mdr;
         }
     }
 }
---
+++ Schema.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker
+{
+    struct User
+    {
+        public int id;
+        public string username;
+        public string password;
+
+        public void updateUser(int id, string username, string password)
+        {
+            this.id = id;
+            this.username = username;
+            this.password = password;
+        }
+    }
+
+    struct Expense
+    {
+        public int id;
+        public int userId;
+        public string name;
+        public string tag;
+        public double amount;
+        public DateTime date;
+
+        public void updateExpense(int id, string name, string tag, double amount, DateTime date, int userId)
+        {
+            this.id = id;
+            this.userId = userId;
+            this.name = name;
+            this.tag = tag;
+            this.amount = amount;
+            this.date = date;
+        }
+    }
+}
