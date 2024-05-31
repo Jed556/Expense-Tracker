99a0d374fecd2502d6852871a447e5ebe8f1f598
Jed556 <jguiriba11@gmail.com>
2024-05-31 16:27:54 +0800

    refactor: Update environment

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyEnterpriseSystem")]
+[assembly: AssemblyTitle("ExpenseTracker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyEnterpriseSystem")]
+[assembly: AssemblyProduct("ExpenseTracker")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
