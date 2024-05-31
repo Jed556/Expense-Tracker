6f6b1f40653b82f222f233cf7f1a94c7259bd0ac
Jed556 <jguiriba11@gmail.com>
2024-06-01 05:27:16 +0800

    feat: Add form switching handler

---
+++ Functions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpenseTracker
+{
+    public static class Functions {
+        public static void SwitchWindow(Form newForm)
+        {
+            List<Form> openForms = new List<Form>();
+            foreach (Form form in Application.OpenForms)
+                if (form.Name != newForm.Name)
+                openForms.Add(form);
+
+            foreach (Form form in openForms)
+            {
+                form.Close();
+            }
+
+            newForm.Show();
+        }
+    }
+}
